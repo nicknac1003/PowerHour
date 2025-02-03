f637a6a4ec39578e9cd28cab56bca195982eebd6
Dhutchison00 <59940070+Dhutchison00@users.noreply.github.com>
2025-02-02 20:33:58 -0500

    big enemy AI improvments

--- PowerHour/Assets/Scripts/Enemies/bottle.cs
+++ PowerHour/Assets/Scripts/Enemies/bottle.cs
@@ -8,42 +8,36 @@ class bottle : MonoBehaviour
     private AudioSource audioSource;
     public AudioClip shatterSound;
     private MeshRenderer meshRenderer;
-    private bool isShattered = false;
+    private Collider collider;
+    public bool isShattered = false;
     public void Start()
     {
         audioSource = GetComponent<AudioSource>();
         meshRenderer = GetComponent<MeshRenderer>();
-    }
-
-    public void Update()
-    {
-        //if y within .1 of 0, destroy the bottle
-        if (transform.position.y < 0.1 && !isShattered)
-        {
-            shatter();
-        }
+        collider = GetComponent<Collider>();
     }
 
     //on collision with player, destroy the bottle and deal damage to the player
     public void OnTriggerEnter(Collider other)
     {
-        Debug.Log("Bottle collided with " + other.gameObject.tag);
+        //Debug.Log("Bottle collided with " + other.gameObject.tag);
         // IDamageable other = collision.gameObject.GetComponent<IDamageable>();
         if (other.gameObject.tag == "Player Hitbox")
         {
             other.gameObject.GetComponentInParent<PlayerAnimationController>().TakeDamage(10);
             other.gameObject.GetComponentInParent<PlayerController>().TakeDamage(10);
-            shatter();
         }
+        shatter();
 
     }
 
-    public void shatter()
+    private void shatter()
     {
         isShattered = true;
         audioSource.clip = shatterSound;
         audioSource.Play();
         meshRenderer.enabled = false;
+        collider.enabled = false;
         GameObject effect = Instantiate(shatter_effect, transform.position, transform.rotation);
         effect.GetComponent<ParticleSystem>().Play();
         Destroy(gameObject, 3f);
---
+++ PowerHour/Assets/Scripts/WaveManager.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class WaveManager : MonoBehaviour
+{
+    public int waveNumber = 0;
+    public int enemiesRemaining = 0;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
